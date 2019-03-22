d6ef4d552daaebf540ba10e495cb9e9c7dcd319b
Roland <rolanddegrabla@gmail.com>
2019-03-22 16:08:49 +0100

    Puszczaj

---
+++ gitowy/src/main/java/pw/robac/gitowy/GitowyApplication.java
@@ -0,0 +1,13 @@
+package pw.robac.gitowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitowyApplication.class, args);
+	}
+
+}
